20c9dab7b20066de92c6d682a586e15f359e072e
Himanshu Kumawat <013himanshu@gmail.com>
2020-12-21 17:41:15 +0530

    added dark mode

--- app/src/main/java/com/example/darkmode/MainActivity.kt
+++ app/src/main/java/com/example/darkmode/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.darkmode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.databinding.DataBindingUtil
+import com.example.darkmode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        binding.switch1.setOnCheckedChangeListener{ _, isChecked ->
+            if (isChecked) {
+                Log.i("Switch", "ON")
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            else {
+                Log.i("Switch", "OFF")
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 }
