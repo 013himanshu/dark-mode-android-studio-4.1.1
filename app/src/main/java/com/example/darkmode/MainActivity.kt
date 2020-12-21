895ad0b02bec97c8501f66fb85f3dc53955341b8
Himanshu Kumawat <013himanshu@gmail.com>
2020-12-22 01:37:08 +0530

    code reformated

--- app/src/main/java/com/example/darkmode/MainActivity.kt
+++ app/src/main/java/com/example/darkmode/MainActivity.kt
@@ -12,7 +12,7 @@ import com.example.darkmode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
